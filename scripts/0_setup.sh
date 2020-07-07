c660a1fc928829b01405664612a6d2841ef33899
Oleksandr Chychkan <chychkan@gmail.com>
2020-07-06 23:06:13 -0700

    Make shallow copy of DFL repo to speedup setup process

--- scripts/0_setup.sh
+++ scripts/0_setup.sh
@@ -6,7 +6,7 @@ mkdir -p workspace
 
 if [ ! -d .dfl/DeepFaceLab ]; then
   echo "Cloning DeepFaceLab"
-  git clone "https://github.com/iperov/DeepFaceLab.git" .dfl/DeepFaceLab
+  git clone --depth 1 "https://github.com/iperov/DeepFaceLab.git" .dfl/DeepFaceLab
 fi
 
 if [ ! -d .dfl/env ]; then
