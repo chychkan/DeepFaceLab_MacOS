2c0c8a956aa27dbe0534bbc10483fd96cd73925a
Smiril <Smiril@users.noreply.github.com>
2022-09-26 10:11:28 +0200

    development reset

--- scripts/5.4_data_dst_mask_editor.sh
+++ scripts/5.4_data_dst_mask_editor.sh
@@ -2,7 +2,5 @@
 cd "$(dirname $0)/.."
 source scripts/env.sh
 
-$DFL_PYTHON "$DFL_SRC" labelingtool edit_mask \
-    --input-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --confirmed-dir "$DFL_WORKSPACE/data_dst/aligned_confirmed" \
-    --skipped-dir "$DFL_WORKSPACE/data_dst/aligned_skipped"
+$DFL_PYTHON "$DFL_SRC" facesettool enhance \
+    --input-dir "$DFL_WORKSPACE/data_dst/aligned" 
