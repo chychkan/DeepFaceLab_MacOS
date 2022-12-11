67ce9bea051a81120d58478da51d2dad8cd68c2d
Smiril <Smiril@users.noreply.github.com>
2022-12-11 14:19:42 +0100

    Update 5_data_dst_extract_faces_S3FD_best_GPU.sh

--- scripts/5_data_dst_extract_faces_S3FD_best_GPU.sh
+++ scripts/5_data_dst_extract_faces_S3FD_best_GPU.sh
@@ -5,6 +5,5 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_SRC" extract \
     --input-dir "$DFL_WORKSPACE/data_dst" \
     --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --detector s3fd \
-    --debug-dir "$DFL_WORKSPACE/data_dst/aligned_debug"
+    --detector s3fd 
 
