eee6ddce010d0eaa457c63ee5cdd895f5dbce073
Smiril <Smiril@users.noreply.github.com>
2022-12-25 18:57:17 +0100

    Update 7_convert_AMP.sh

--- scripts/7_convert_AMP.sh
+++ scripts/7_convert_AMP.sh
@@ -2,9 +2,10 @@
 cd "$(dirname $0)/.."
 source scripts/env.sh
 
-$DFL_PYTHON "$DFL_SRC" convert \
+$DFL_PYTHON "$DFL_SRC" merge \
     --input-dir "$DFL_WORKSPACE/data_dst" \
     --output-dir "$DFL_WORKSPACE/data_dst/merged" \
+    --output-mask-dir "$DFL_WORKSPACE/data_dst/masked" \
     --aligned-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --model-dir "$DFL_WORKSPACE/model" \
     --model AMP
