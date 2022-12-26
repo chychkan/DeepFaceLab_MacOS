90d57b0ff853f7dabd94f7b6a41a13e68f602f70
Smiril <Smiril@users.noreply.github.com>
2022-12-26 13:56:13 +0100

    Update 7_convert_Quick96.sh

--- scripts/7_convert_Quick96.sh
+++ scripts/7_convert_Quick96.sh
@@ -4,9 +4,9 @@ source scripts/env.sh
 
 $DFL_PYTHON "$DFL_SRC" merge \
     --input-dir "$DFL_WORKSPACE/data_src" \
-    --output-dir "$DFL_WORKSPACE/data_dst/merged" \
-    --output-mask-dir "$DFL_WORKSPACE/data_dst/masked" \
-    --aligned-dir "$DFL_WORKSPACE/data_dst/aligned" \
+    --output-dir "$DFL_WORKSPACE/data_src/merged" \
+    --output-mask-dir "$DFL_WORKSPACE/data_src/masked" \
+    --aligned-dir "$DFL_WORKSPACE/data_src/aligned" \
     --model-dir "$DFL_WORKSPACE/model" \
     --model Quick96
     
