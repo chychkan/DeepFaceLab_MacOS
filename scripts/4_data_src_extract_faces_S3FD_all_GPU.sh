f72e7e8898ed95da295cad75dc9472cd6ce621e1
Smiril <Smiril@users.noreply.github.com>
2022-12-26 13:52:38 +0100

    Development Reset

--- scripts/4_data_src_extract_faces_S3FD_all_GPU.sh
+++ scripts/4_data_src_extract_faces_S3FD_all_GPU.sh
@@ -5,6 +5,5 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_MAIN" extract \
     --input-dir "$DFL_WORKSPACE/data_src" \
     --output-dir "$DFL_WORKSPACE/data_src/aligned" \
-    --multi-gpu \
     --detector s3fd
 
