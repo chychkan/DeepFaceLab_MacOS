6f9bf9709f0c1f2217cc3532a257ff720936ad9c
Smiril <Smiril@users.noreply.github.com>
2022-12-26 14:14:44 +0100

    Update 8_converted_to_avi.sh

--- scripts/8_converted_to_avi.sh
+++ scripts/8_converted_to_avi.sh
@@ -3,7 +3,7 @@ cd "$(dirname $0)/.."
 source scripts/env.sh
 
 $DFL_PYTHON "$DFL_SRC" videoed video-from-sequence \
-    --input-dir "$DFL_WORKSPACE/data_dst/aligned" \
+    --input-dir "$DFL_WORKSPACE/data_src/aligned" \
     --output-file "$DFL_WORKSPACE/result.avi" \
     --include-audio \
     --reference-file "$DFL_WORKSPACE/data_dst.*"
