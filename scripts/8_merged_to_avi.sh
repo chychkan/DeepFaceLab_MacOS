a90fba7722d0a40910f0174b2a1971acad1f2b8e
Smiril <Smiril@users.noreply.github.com>
2022-12-26 14:49:58 +0100

    Create 8_merged_to_avi.sh

---
+++ scripts/8_merged_to_avi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)/.."
+source scripts/env.sh
+
+$DFL_PYTHON "$DFL_SRC" videoed video-from-sequence \
+    --input-dir "$DFL_WORKSPACE/data_src/aligned" \
+    --output-file "$DFL_WORKSPACE/result.avi" \
+    --include-audio \
+    --reference-file "$DFL_WORKSPACE/data_dst.*"
+
