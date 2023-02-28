f404baebcbb2f4aafe1f5140d6ed1d94b6cc0806
ngardelis <118944371+ngardelis@users.noreply.github.com>
2023-02-28 08:45:54 +0200

    Update 6_train_SAEHD.sh

--- scripts/6_train_SAEHD.sh
+++ scripts/6_train_SAEHD.sh
@@ -2,7 +2,7 @@
 cd "$(dirname $0)/.."
 source scripts/env.sh
 
-python "$DFL_MAIN" train \
+python3.9 "$DFL_MAIN" train \
   --training-data-src-dir "$WORKSPACE/data_src/aligned" \
   --training-data-dst-dir "$WORKSPACE/data_dst/aligned" \
   --model-dir "$WORKSPACE/model" \
