69bd917549034e9cf7540951e013b6e157963ca3
Smiril <Smiril@users.noreply.github.com>
2024-02-26 18:47:29 +0100

    Update 5_data_dst_extract_faces_MT_all_GPU.sh

--- scripts/5_data_dst_extract_faces_MT_all_GPU.sh
+++ scripts/5_data_dst_extract_faces_MT_all_GPU.sh
@@ -5,6 +5,5 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_MAIN" extract \
     --input-dir "$DFL_WORKSPACE/data_dst" \
     --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --multi-gpu \
     --detector mt \
     --debug-dir "$DFL_WORKSPACE/data_dst/aligned_debug"
