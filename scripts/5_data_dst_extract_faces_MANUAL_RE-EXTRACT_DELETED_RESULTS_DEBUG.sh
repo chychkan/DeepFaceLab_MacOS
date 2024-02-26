a73067270d1330495f4c5f8271f5c15b6044e336
Smiril <Smiril@users.noreply.github.com>
2024-02-26 18:46:51 +0100

    Update 5_data_dst_extract_faces_MANUAL_RE-EXTRACT_DELETED_RESULTS_DEBUG.sh

--- scripts/5_data_dst_extract_faces_MANUAL_RE-EXTRACT_DELETED_RESULTS_DEBUG.sh
+++ scripts/5_data_dst_extract_faces_MANUAL_RE-EXTRACT_DELETED_RESULTS_DEBUG.sh
@@ -5,7 +5,6 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_SRC" extract \
     --input-dir "$DFL_WORKSPACE/data_dst" \
     --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --multi-gpu \
     --detector manual \
     --manual-output-debug-fix \
     --debug-dir "$DFL_WORKSPACE/data_dst/aligned_debug"
