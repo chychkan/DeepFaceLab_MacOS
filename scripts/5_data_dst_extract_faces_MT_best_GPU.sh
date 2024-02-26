3419dd3dc7b3e61ea43ca077ec04b5a755839ae8
Smiril <Smiril@users.noreply.github.com>
2024-02-26 18:47:50 +0100

    Update 5_data_dst_extract_faces_MT_best_GPU.sh

--- scripts/5_data_dst_extract_faces_MT_best_GPU.sh
+++ scripts/5_data_dst_extract_faces_MT_best_GPU.sh
@@ -4,7 +4,5 @@ source scripts/env.sh
 
 $DFL_PYTHON "$DFL_MAIN" extract \
     --input-dir "$DFL_WORKSPACE/data_dst" \
-    --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --multi-gpu \
     --detector mt \
     --debug-dir "$DFL_WORKSPACE/data_dst/aligned_debug"
