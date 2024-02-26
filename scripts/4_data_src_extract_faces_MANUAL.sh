06972724c2010a8b0a194c042f941a8fa851cad4
Smiril <Smiril@users.noreply.github.com>
2024-02-26 18:45:22 +0100

    Update 4_data_src_extract_faces_MANUAL.sh

--- scripts/4_data_src_extract_faces_MANUAL.sh
+++ scripts/4_data_src_extract_faces_MANUAL.sh
@@ -5,7 +5,6 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_MAIN" extract \
     --input-dir "$DFL_WORKSPACE/data_src" \
     --output-dir "$DFL_WORKSPACE/data_src/aligned" \
-    --multi-gpu \
     --detector manual \
     --debug-dir "$DFL_WORKSPACE/data_src/aligned_debug"
 
