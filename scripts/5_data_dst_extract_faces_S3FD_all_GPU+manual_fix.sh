f9230fe61fce49cd37ed34565b0c453635e7bdd4
Smiril <Smiril@users.noreply.github.com>
2024-02-26 18:48:09 +0100

    Update 5_data_dst_extract_faces_S3FD_all_GPU+manual_fix.sh

--- scripts/5_data_dst_extract_faces_S3FD_all_GPU+manual_fix.sh
+++ scripts/5_data_dst_extract_faces_S3FD_all_GPU+manual_fix.sh
@@ -6,6 +6,5 @@ $DFL_PYTHON "$DFL_MAIN" extract \
     --input-dir "$DFL_WORKSPACE/data_dst" \
     --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --detector s3fd \
-    --multi-gpu \
     --manual-fix
 
