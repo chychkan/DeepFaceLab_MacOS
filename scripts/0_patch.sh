dbfb19b9b81d07574acb70f57a3b47eed02bcca5
Smiril <Smiril@users.noreply.github.com>
2024-02-27 15:22:12 +0100

    Patch for upgrade

---
+++ scripts/0_patch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)/.."
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+set -e
+
+source .dfl/deep/bin/activate
+
+set -e
+
+python3.11 -m pip install --upgrade pip numpy opencv-python colorama tqdm future tensorflow-macos scipy
+
+source .dfl/env/bin/activate
+
+set -e
+
+python3.10 -m pip install --upgrade pip numpy opencv-python colorama tqdm future tensorflow-macos scipy
+
+cp -R .dfl/ffmpeg/ffmpeg .dfl/deep/lib/python$(version)/site-packages/
+
+echo "Done."
