75556cae87d169842e446bf6656f0995843b632d
Smiril <Smiril@users.noreply.github.com>
2024-02-27 18:44:38 +0100

    Update 8_converted_to_mov_lossless+alpha.sh

--- scripts/8_converted_to_mov_lossless+alpha.sh
+++ scripts/8_converted_to_mov_lossless+alpha.sh
@@ -5,7 +5,7 @@ source scripts/env.sh
 $DFL_PYTHON "$DFL_MAIN" videoed video-from-sequence \
     --input-dir "$DFL_WORKSPACE/data_dst/merged" \
     --output-file "$DFL_WORKSPACE/result.mov" \
-    --reference-file "$DFL_WORKSPACE/data_src.*" \
+    --reference-file "$DFL_WORKSPACE/data_dst.*" \
     --include-audio \
     --lossless
 
