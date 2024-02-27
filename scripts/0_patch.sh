72a3cc4d601e4b012db7d0f01d7a8afd0474d6be
Smiril <Smiril@users.noreply.github.com>
2024-02-27 16:18:31 +0100

    Update 0_patch.sh

--- scripts/0_patch.sh
+++ scripts/0_patch.sh
@@ -14,7 +14,7 @@ source .dfl/env/bin/activate
 
 set -e
 
-python3.10 -m pip install --upgrade pip numpy==1.22.0 numexpr opencv-python colorama tqdm future tensorflow-macos scipy pillow PyQt5 onnx protobuf>=3.12.2
+python3.10 -m pip install --upgrade pip numpy==1.23.3 numexpr opencv-python colorama tqdm future tensorflow-macos scipy pillow PyQt5 onnx protobuf>=3.12.2
 
 cp -R .dfl/ffmpeg/ffmpeg .dfl/deep/lib/python3.11/site-packages/
 
