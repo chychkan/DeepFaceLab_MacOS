73f0b2849dfabd43cd46352dffef3b9e254aee32
Smiril <Smiril@users.noreply.github.com>
2024-02-27 18:44:57 +0100

    Update 8_converted_to_mp4.sh

--- scripts/8_converted_to_mp4.sh
+++ scripts/8_converted_to_mp4.sh
@@ -6,5 +6,5 @@ $DFL_PYTHON "$DFL_MAIN" videoed video-from-sequence \
     --input-dir "$DFL_WORKSPACE/data_dst/merged" \
     --output-file "$DFL_WORKSPACE/result.mp4" \
     --include-audio \
-    --reference-file "$DFL_WORKSPACE/data_src.*"
+    --reference-file "$DFL_WORKSPACE/data_dst.*"
 
