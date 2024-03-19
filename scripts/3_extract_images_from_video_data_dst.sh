3215437b587259819664ae7e155d47f8e87362ce
Smiril <Smiril@users.noreply.github.com>
2024-03-19 19:18:24 +0100

    Update 3_extract_images_from_video_data_dst.sh

--- scripts/3_extract_images_from_video_data_dst.sh
+++ scripts/3_extract_images_from_video_data_dst.sh
@@ -2,6 +2,6 @@
 cd "$(dirname $0)/.."
 source scripts/env.sh
 
-python "$DFL_MAIN" videoed extract-video \
+$DFL_PYTHON "$DFL_MAIN" videoed extract-video \
   --input-file "$WORKSPACE/data_dst.*" \
   --output-dir "$WORKSPACE/data_dst"
